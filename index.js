4bea821d643f0f43511010e8668732085182411a
hazratgs <hazratgs@gmail.com>
2017-06-04 00:36:23 +0300

    Добавлена в зависимость библиотека url-parse
    С помощью неё будем определять тип подключения (http/https)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const urlParse = require('url-parse');
 
 module.exports = (url, options) => {
-
+    let parse = urlParse(url); 
 };
