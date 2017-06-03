35ac7790641920e8afcefb4d0f3f3bf8b2a5c8c2
hazratgs <hazratgs@gmail.com>
2017-06-04 01:15:25 +0300

    Базовый функционал загрузка файлов реализован
    Было выполнено следующее
    1. Подключили модуль md5 для составления относительно уникальных имен файлов
    2. Формируем имя файла, если не передан в options
    3. Используя оба протокала (http/https) реализована базовая загрузка файла в директорию по умолчанию (./) либо в определенную в options.path
    4. Возвращаем name файла
    5. Error при отличном от https/http протоколе

--- index.js
+++ index.js
@@ -2,7 +2,40 @@ const http = require('http');
 const https = require('https');
 const fs = require('fs');
 const urlParse = require('url-parse');
+const md5 = require('md5');
 
 module.exports = (url, options) => {
-    let parse = urlParse(url); 
+    let parse = urlParse(url);
+
+    // Тип файла
+    let type = parse.pathname.split('.');
+
+    // Имя файла
+    let name = !options.name ? md5(new Date()) + '.' + type[type.length - 1] : options.name;
+
+    // Дириктория, куда будет сохранен файл
+    let fileStream = fs.createWriteStream(options.path + name);
+
+    // Загружаем файл
+    return new Promise((resolve, reject) => {
+
+        // Определяем протокол соединения
+        if (parse.protocol === 'https:'){
+            https.get(url, response => {
+                response.pipe(fileStream);
+
+                // На выход отдаем название файла
+                resolve(name)
+            })
+        } else if (parse.protocol === 'http:'){
+            http.get(url, response => {
+                response.pipe(fileStream);
+
+                // На выход отдаем название файла
+                resolve(name)
+            })
+        } else {
+            reject('Ошибка, не определен протокол соединения')
+        }
+    });
 };
