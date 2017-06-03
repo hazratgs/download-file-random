f319de04dc7a82217e113eb141d803d307c81cd2
hazratgs <hazratgs@gmail.com>
2017-06-04 01:09:11 +0300

    Базовый функционал загрузка файлов реализован
    Было выполнено следующее
    1. Подключили модуль md5 для составления относительно уникальных имен
    файлов
    2. Формируем имя файла, если не передан в options
    3. Используя оба протокала (http/https) реализована базовая загрузка
    файла в директорию по умолчанию (./) либо в определенную в options.path
    4. Возвращаем name файла
    5. Error при отличном от https/http протоколе

--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
 const downloader = require('../index');
 
-downloader('https://scontent.cdninstagram.com/t51.2885-15/e35/18888672_1318212121630783_2222031790493663232_n.jpg');
+downloader('https://scontent.cdninstagram.com/t51.2885-15/e35/18888672_1318212121630783_2222031790493663232_n.jpg', {
+    path: ''
+}).then(res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err)
+})
